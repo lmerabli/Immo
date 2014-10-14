30a79ccad0f34ea56e5ca8b8a399a2eae6b26fda
FlorianDeParis <florian.tournay@hotmail.fr>
2014-10-14 16:29:58 +0200

    tests d'affichage

---
+++ wp-content/themes/immotheme/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ESGI Immobilier</title>
+		<!--Variable Wordpress du css par défaut-->
+		<link rel"stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+		<?php wp_head(); ?>
+	</head>
--- wp-content/themes/immotheme/index.php
+++ wp-content/themes/immotheme/index.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>ESGI Immobilier</title>
-		<!--Variable Wordpress du css par défaut-->
-		<link rel"stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
-		<?php wp_head(); ?>
-	</head>
-	<body>
+<?php get_header(); ?>
+	<div class="test">
 		<div class="banner">
 			<!-- variable wordpress du nom du site-->
 			<h1><?php bloginfo('name'); ?></h1>
 			<!-- variable wordpress du slogan du site-->
 			<p><?php bloginfo('description'); ?></p>
 		</div>
-		<?php wp_footer(); ?>
-		<form method="get" id="form" action="<?php bloginfo('url'); ?>/">
-		  	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s">
-		 	<input type="submit" id="submit">
-		</form>
-
-	</body>
-</html>
+	</div>
+<?php
+get_sidebar();
+get_footer();
