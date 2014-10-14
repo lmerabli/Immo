e5f87fa664a0c1d2c341f159bdb220517a51ae5d
FlorianDeParis <florian.tournay@hotmail.fr>
2014-10-14 23:46:58 +0200

    structuration du theme WP

--- wp-content/themes/immotheme/index.php
+++ wp-content/themes/immotheme/index.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
-	<div class="test">
+	<div>
 		<div class="banner">
-			<!-- variable wordpress du nom du site-->
-			<h1><?php bloginfo('name'); ?></h1>
-			<!-- variable wordpress du slogan du site-->
-			<p><?php bloginfo('description'); ?></p>
+				<!-- variable wordpress du nom du site-->
+				<h1><?php bloginfo('name'); ?></h1>
+				<!-- variable wordpress du slogan du site-->
+				<p><?php bloginfo('description'); ?></p>
 		</div>
-	</div>
-<?php
-get_sidebar();
-get_footer();
+		<div class="content">
+			<!--COLONNE GAUCHE-->
+			<div class="body">
+				<p>Ceci est un contenu test</p>
+			</div>
+			<!--COLONNE DROITE-->
+			<div class="side">
+				<?php get_sidebar(); ?>
+			</div>
+				<?php get_footer(); ?>
