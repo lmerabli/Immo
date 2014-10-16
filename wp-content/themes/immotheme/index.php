4c3cf59533aad4e81adb9d0e3ce940d09c5a1337
FlorianDeParis <florian.tournay@hotmail.fr>
2014-10-16 12:12:10 +0200

    Tests loop et images

--- wp-content/themes/immotheme/index.php
+++ wp-content/themes/immotheme/index.php
@@ -14,7 +14,7 @@
 			<!--COLONNE GAUCHE-->
 			<div class="body">
 				<p>Ceci est un contenu test</p>
-				<?php get_template_part( 'loop', 'index' );    ?>
+				<?php get_template_part( 'loop', 'index' ); ?>
 			</div>
 			<!--COLONNE DROITE-->
 			<div class="side">
