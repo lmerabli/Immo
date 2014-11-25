378ea77b1d831d4fb483c76a5867e8eacdeb9c20
FlorianDeParis <florian.tournay@hotmail.fr>
2014-11-25 22:25:57 +0100

    ajout des metas dans les resultats

--- wp-config.php
+++ wp-config.php
@@ -16,8 +16,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-//define('DB_NAME', 'WordPress4');
-define('DB_NAME', 'esgi_projet_wordpress');
+define('DB_NAME', 'WordPress4');
+//define('DB_NAME', 'esgi_projet_wordpress');
 
 /** MySQL database username */
 
