043f75c796fa899b818d634cc64e20302f80a67c
lmerabli <lmerabli@gmail.com>
2014-11-25 18:04:37 +0100

    colorpicker

--- wp-content/themes/immotheme/loop.php
+++ wp-content/themes/immotheme/loop.php
@@ -17,7 +17,7 @@
 
         query_posts( array (
             'post_type' => 'immo',
-            'posts_per_page' => 4
+            'posts_per_page' => 0
             ) );
 
         while (have_posts()) : the_post(); ?>
